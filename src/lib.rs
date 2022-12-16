c9e7c925d1147d84a8260226733236c0caf7ceae
Daniel R <nzdanron@gmail.com>
2022-12-17 08:34:06 +1300

    Started writing the run method for the BrainfuckProgram Struct. Written the first four instructions IncreasePointer, DecreasePointer, IncreaseValue, DescreaseValue and written a unit test that checks that they work

--- src/lib.rs
+++ src/lib.rs
@@ -12,12 +12,12 @@ pub enum BrainfuckInstructions {
 }
 
 /// Const for how large the array is
-const MEMORY_ARRAY_LENGTH: usize = 256;
+const MEMORY_ARRAY_LENGTH: usize = 30000;
 
 /// Struct to represent Brainfuck program
 pub struct BrainfuckProgram {
     /// The array of memory that the program modifies
-    memory_array: [u32; MEMORY_ARRAY_LENGTH],
+    memory_array: [u8; MEMORY_ARRAY_LENGTH],
 
     /// The instructions for the program to run
     instructions: Vec<BrainfuckInstructions>,
@@ -32,7 +32,10 @@ pub struct BrainfuckProgram {
     return_address: usize,
 }
 
+use BrainfuckInstructions::*;
+
 impl BrainfuckProgram {
+
     /// Creates a new BrainfuckProgram
     ///
     /// Parameters
@@ -46,6 +49,44 @@ impl BrainfuckProgram {
             return_address: 0,
         }
     }
+
+    /// Runs the brainfuck program
+    pub fn run(&mut self) {
+        // This will run the program until the end of the instructions is run. We can't just loop
+        // through them as loops will not work
+        while self.program_counter != self.instructions.len() {
+            match &self.instructions[self.program_counter] {
+                // > Instruction
+                IncreasePointer => {
+                    if self.pointer < (MEMORY_ARRAY_LENGTH - 1) {
+                        self.pointer += 1;
+                    }
+                },
+                // < Instruction
+                DecreasePointer => {
+                    if self.pointer != 0 {
+                        self.pointer -= 1;
+                    }
+                },
+                // + Instruction
+                IncreaseValue => {
+                    if self.memory_array[self.pointer] != u8::MAX {
+                        self.memory_array[self.pointer] += 1;
+                    }
+                }
+                // - Instruction
+                DecreaseValue => {
+                    if self.memory_array[self.pointer] != 0 {
+                        self.memory_array[self.pointer] -= 1;
+                    }
+                }
+                _ => (),
+            }
+
+            // Increasing the program counter to move onto the next instruction on the next loop
+            self.program_counter += 1;
+        }
+    } 
 }
 
 #[cfg(test)]
@@ -59,7 +100,10 @@ mod tests {
         IncreaseValue,   // +
         PrintCell,       // .
         WriteToCell,     // ,
-    };
+    }; 
+
+    use super::MEMORY_ARRAY_LENGTH;
+
     use super::BrainfuckProgram;
 
     #[test]
@@ -79,5 +123,35 @@ mod tests {
         let brainfuck_program = BrainfuckProgram::new(instructions.clone());
 
         assert_eq!(brainfuck_program.instructions, instructions)
+    }
+
+    #[test]
+    /// Tests if IncreasePointer, DecreasePointer, IncreaseValue, and DecreaseValue Instructions 
+    /// works
+    fn first_four_instructions_works() {
+        // The instruction set to test
+        let instructions = vec![
+            IncreaseValue,
+            IncreasePointer,
+            IncreaseValue,
+            IncreaseValue,
+            DecreasePointer,
+            DecreaseValue,
+            IncreasePointer,
+            IncreasePointer,
+            IncreaseValue
+        ];
+
+        // Creating the Brainfuck program and running it
+        let mut program = BrainfuckProgram::new(instructions);
+        program.run();
+
+        // Writing the expected state of the memory array
+        let mut expected_array: [u8; MEMORY_ARRAY_LENGTH] = [0; MEMORY_ARRAY_LENGTH];
+        expected_array[1] = 2;
+        expected_array[2] = 1;
+
+        // Testing if they are the same
+        assert!(expected_array.iter().eq(program.memory_array.iter()))
     }
 }

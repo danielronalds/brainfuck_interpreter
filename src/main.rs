624c75aaa8c4d342bce8b16ee4d907d27f873d73
Daniel R <nzdanron@gmail.com>
2022-12-17 23:49:41 +1300

    Created a rough function that reads a file and extracts brainfuck instructions from

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use brainfuck_interpreter::BrainfuckInstructions::{
+    self,
     BeginLoop,       // [
     DecreasePointer, // <
     DecreaseValue,   // -
@@ -10,17 +11,50 @@ use brainfuck_interpreter::BrainfuckInstructions::{
 };
 use brainfuck_interpreter::BrainfuckProgram;
 
-fn main() {
-    let instructions = vec![
-        IncreasePointer,
-        DecreasePointer,
-        IncreaseValue,
-        DecreaseValue,
-        BeginLoop,
-        EndLoop,
-        PrintCell,
-        WriteToCell,
-    ];
+use clap::Parser;
 
-    let _program = BrainfuckProgram::new(instructions);
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+#[derive(Parser, Debug)]
+/// A brainfuck interpreter written in rust!
+#[clap(author, version, about)]
+pub struct Args {
+    /// The brainfuck program file
+    filename: String, 
+}
+
+fn main() {
+    let args = Args::parse();
+
+    let instructions = file_to_instructions(args.filename);
+
+    let mut program = BrainfuckProgram::new(instructions);
+
+    program.run();
+}
+
+fn file_to_instructions(filename: String) -> Vec<BrainfuckInstructions> {
+    let mut instructions_vec = Vec::new();
+
+    let file = File::open(filename).expect("Couldn't open file");
+
+    for line in BufReader::new(file).lines() {
+        for char in line.expect("Couldn't read line").chars() {
+            match char {
+                '>' => instructions_vec.push(IncreasePointer),
+                '<' => instructions_vec.push(DecreasePointer),
+                '+' => instructions_vec.push(IncreaseValue),
+                '-' => instructions_vec.push(DecreaseValue),
+                '[' => instructions_vec.push(BeginLoop),
+                ']' => instructions_vec.push(EndLoop),
+                '.' => instructions_vec.push(PrintCell),
+                ',' => instructions_vec.push(WriteToCell),
+                // Ignore all other chars
+                _ => continue
+            }
+        }
+    }
+
+    instructions_vec
 }
